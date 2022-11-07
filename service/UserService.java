2aee479526f551019365dad12dd96de7e68b7b32
Andrey33392 <114940742+Andrey33392@users.noreply.github.com>
2022-11-07 10:48:03 +0300

    ver1

---
+++ service/UserService.java
@@ -0,0 +1,20 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface UserService {
+    @Transactional
+    void add(User user);
+
+    void add(User user, Car car);
+    List<User> listUsers();
+
+    @Transactional(readOnly = true)
+    User getUserByCar(String model, int series);
+
+
+}
